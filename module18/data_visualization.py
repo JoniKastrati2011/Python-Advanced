c497c0bdc32f4a26c6814de12e07f85a706ff1eb
JoniKastrati2011 <131296411+JoniKastrati2011@users.noreply.github.com>
2025-11-11 19:11:42 +0100

    .

--- module18/data_visualization.py
+++ module18/data_visualization.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-books_df = pd.read_csv('bestsellers_with_categories_2022_03_27.csv')
+books_df = pd.read_csv('Test.csv')
 
 st.title("Bestselling books Analysis")
 st.write("This app analyses the Amazon Top selling books from 2009 to 2022")
