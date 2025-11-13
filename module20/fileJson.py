de9c473a6fea185b9d2f56875e058239772fcd52
JoniKastrati2011 <131296411+JoniKastrati2011@users.noreply.github.com>
2025-11-13 19:11:51 +0100

    .

---
+++ module20/fileJson.py
@@ -0,0 +1,20 @@
+{
+  "name": "John Doe",
+  "age": 30,
+  "adress": {
+    "street": "123 Main St",
+    "city": "Anytown",
+    "state": "CA",
+    "postal_code": "12345"
+  },
+  "Contacts": [
+    {
+      "type": "email",
+      "value":"john.coe@gmail.com"
+    },
+    {
+      "type" : "phone",
+      "value": "555-4--382"
+    }
+  ]
+}
