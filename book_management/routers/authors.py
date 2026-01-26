09ccc5baebececc19cbddb9379fb64cd5a677daa
JoniKastrati2011 <131296411+JoniKastrati2011@users.noreply.github.com>
2026-01-26 19:13:04 +0100

    .

---
+++ book_management/routers/authors.py
@@ -0,0 +1,18 @@
+import sqlite3
+from typing import List
+from fastapi import Depends, FastAPI, HTTPException, status, APIRouter
+from models.author import Author, AuthorCreate
+from database import get_db_connection
+from auth.security import get_api_key
+
+router = APIRouter()
+
+@router.get("/", response_model=List[Author])
+def get_authors():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, name FROM authors")
+    authors = cursor.fetchall()
+    conn.close()
+    return [{"id": author[0], "name": author[1]} for author in authors]
+    
