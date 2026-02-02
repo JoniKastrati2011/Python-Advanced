a90839a5b3306bd3fddcf6b66b4da84a2093a7c1
JoniKastrati2011 <131296411+JoniKastrati2011@users.noreply.github.com>
2026-02-02 19:12:10 +0100

    .

--- book_management/routers/authors.py
+++ book_management/routers/authors.py
@@ -1,9 +1,11 @@
 import sqlite3
 from typing import List
 from fastapi import Depends, FastAPI, HTTPException, status, APIRouter
-from models.author import Author, AuthorCreate
-from database import get_db_connection
-from auth.security import get_api_key
+
+from book_management.auth.security import get_api_key
+from book_management.models import author
+from book_management.models.author import Author, AuthorCreate
+from book_management.database import get_db_connection
 
 router = APIRouter()
 
@@ -15,4 +17,62 @@ def get_authors():
     authors = cursor.fetchall()
     conn.close()
     return [{"id": author[0], "name": author[1]} for author in authors]
-    
+
+@router.post("/",response_model=Author)
+def create_author(
+            author: AuthorCreate,
+            _:str = Depends(get_api_key)
+):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    try:
+        cursor.execute("Insert into authors (name) values (?)", (author.name, ))
+        conn.commit()
+        author_id = cursor.lastrowid
+        return Author(id=author_id, name=author.name)
+    except sqlite3.IntegrityError:
+        conn.close()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"The author with name '{author.name}' already exists."
+        )
+    finally:
+        conn.close()
+
+@router.put("/{author.id}",response_model=Author)
+def update_author(
+        author_id: int,
+        author: AuthorCreate,
+        _:str = Depends(get_api_key)
+):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE authors SET name = ? WHERE id = ?",
+        (author.name, author_id)
+    )
+    if cursor.rowcount == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Author with id '{author_id}' not found."
+        )
+    conn.commit()
+    conn.close()
+    return Author(id=author_id, name=author.name)
+
+@router.delete("/{author.id}",response_model=Author)
+def delete_author(
+    author_id: int,
+    _:str = Depends(get_api_key)):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM authors WHERE id = ?", (author_id,))
+    if cursor.rowcount == 0:
+        conn.close()
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Author with id '{author_id}' not found."
+        )
+    conn.commit()
+    conn.close()
+    return Author(id=author_id, name=author.name)
